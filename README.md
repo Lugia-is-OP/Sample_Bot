4a7ea21920b6b5ea8a29f4e9db7155429af76ad8
Your Name <dalaldhairya5@gmail.com>
2021-06-22 12:15:46 +0530

    Added Commands / Made TOKEN Safe

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # Sample_Bot
-Sample Bot to get started with
+Sample Bot to get started with.
+if you are New to Coding
 
 # Installing Part
 Please Do 
-npm install discord.js
+npm install (To INstall Packages)
 
 # Change Token
-Go to config.json File and 
-Place your token there
+Go to .env_sample 
+chnage that file name to .env and PUT YOUR TOKEN THERE
+
+# Change Token
+Go to config.json 
+For Example I have Kept Prefix as ```$```
+and Put you Prefix There 
 
 # Ignoring Stuff
 ignore .gitignore file
--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+// ====================== INPORTING TOKEN =======================
+require('dotenv').config()
+// ====================== START BOT CODE ========================
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const token = require('./config.json')
+const { prefix } = require('./config.json')
 
+// ====================== LISTENING TO CONSOLE ========================
 client.on('ready', () => {
     console.log(`${client.user.tag} is Online!`)
+    // ===== BOT PRESENCE =====
+    client.user.setPresence({
+        activity: {
+            name: "MESSAGE HERE",  // ===== THE MESSAGE SHOWING IN THE PRESENCE =======
+            type: "LISTENING", // ==== PLAYING, WATCHING, LISTENING, STREAMING ====== 
+        }
+    })
 })
+// ====================== LISTENING TO MESSAGES ========================
+client.on('message', async (message) => {
 
-client.login(token)
+    // Returning Message , If it is from guild or Bot or Webhook
+    if (message.author.bot || !message.guild || message.webhookID) return
+    // Returning Message , IF it does not start from Prefix
+    if (!message.content.startsWith(prefix)) return
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase()
+    
+    if(command === "ping"){ // ===== Ping is a Command so When were Anyone Calls (prefix)ping ========
+        message.reply("PONG") // === So Bot wil Reply with PONG ==========
+    }
+})
+// ====================== BOT LOGINING ========================
+client.login(process.env.TOKEN).catch((err) => {
+    console.log("TOKEN Invaild / TOKEN NOT GIVEN ")
+})
